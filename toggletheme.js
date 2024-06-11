be7553e705f54ea9e98d5b9440dbce6291f67655
Matheus Felipe Dos Santos <105308149+theusfelipe@users.noreply.github.com>
2024-06-11 18:12:33 +0000

    trying the last animation in the paragraph below the title

--- toggletheme.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('themeToggle').addEventListener('click', function() {
-  document.body.classList.toggle('dark-theme');
-  document.body.classList.toggle('light-theme');
-});
-
-// Set initial theme
-document.body.classList.add('light-theme');
-
